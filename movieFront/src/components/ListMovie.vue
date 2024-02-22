076df273f0c855a73c7ec908ebeb0f2ea1061f07
Andrea <20213tn008@utez.edu.mx>
2024-02-22 10:44:54 -0600

    Avance

--- movieFront/src/components/ListMovie.vue
+++ movieFront/src/components/ListMovie.vue
@@ -1,159 +1,83 @@
 <template>
   <div>
-    <header
-      class="text-light text-center py-5"
-      style="background-color: #026c80"
-    >
-      <nav
-        class="navbar navbar-expand-lg text-light"
-        style="background-color: #026c80"
-      >
-        <div class="container">
-          <router-link to="/" class="navbar-brand text-light"
-            >Lista de peliculas</router-link
-          >
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <router-link to="/inicio" class="nav-link text-light"
-                  >Inicio</router-link
-                >
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <h1 class="display-4 mt-4">Cartelera</h1>
-    </header>
+    <!-- Navbar -->
+    <b-navbar style="background-color: #026c80;">
+      <b-navbar-brand to="/" class="text-light">AndyLiss Filmsy</b-navbar-brand>
+    </b-navbar>
+    <b-jumbotron class="text-center" style="background-color: #026c80; color: white;">
+      <template #header>
+        Cartelera
+      </template>
+      <template #lead>
+        Descubre una selección impresionante de películas para disfrutar en la comodidad de tu hogar.
+      </template>
+    </b-jumbotron>
+
+    <!-- Sidebar para agregar película -->
+    <b-sidebar id="sidebar-right" title="Agregar Película" right shadow v-model="showSidebar">
+      <b-container>
+        <b-form @submit.prevent="registerMovie">
+          <b-form-group label="Nombre de la película">
+            <b-form-input v-model="newMovie.title"></b-form-input>
+          </b-form-group>
+          <b-form-group label="Género">
+            <b-form-select v-model="newMovie.genre" :options="genresOptions"></b-form-select>
+          </b-form-group>
+          <b-form-group label="Descripción">
+            <b-form-textarea v-model="newMovie.description"></b-form-textarea>
+          </b-form-group>
+          <b-button type="submit" variant="primary">Registrar</b-button>
+        </b-form>
+      </b-container>
+    </b-sidebar>
     <section class="container my-5">
-      <div>
-        <div class="row mb-3">
-          <div class="col-md-4">
-            <input
-              type="text"
-              class="form-control"
-              v-model="searchQuery"
-              placeholder="Buscar por nombre de película"
-            />
-          </div>
-          <div class="col-md-4">
-            <select class="form-control" v-model="selectedGenre">
-              <option value="">Todos los géneros</option>
-              <option v-for="genre in genres" :key="genre" :value="genre">
-                {{ genre }}
-              </option>
-            </select>
-          </div>
-          <div class="col-md-4 text-right">
-            <button class="btn btn-custom text-white" @click="openSidebar">
-              <b-icon
-                icon="camera-reels-fill"
-                font-scale="1"
-                class="text-white"
-              ></b-icon>
-              Agregar
-            </button>
-            <b-sidebar
-              id="sidebar-right"
-              title="Agregar Película"
-              right
-              shadow
-              v-model="showSidebar"
-            >
-              <div class="px-3 py-2">
-                <div class="mb-3">
-                  <label for="movieTitle" class="form-label"
-                    >Nombre de la película:</label
-                  >
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="movieTitle"
-                    v-model="newMovie.title"
-                  />
-                </div>
-                <div class="mb-3">
-                  <label for="movieGenre" class="form-label">Género:</label>
-                  <select
-                    class="form-control"
-                    id="movieGenre"
-                    v-model="newMovie.genre"
-                  >
-                    <option v-for="genre in genres" :key="genre" :value="genre">
-                      {{ genre }}
-                    </option>
-                  </select>
-                </div>
-                <div class="mb-3">
-                  <label for="movieDescription" class="form-label"
-                    >Descripción:</label
-                  >
-                  <textarea
-                    class="form-control"
-                    id="movieDescription"
-                    v-model="newMovie.description"
-                  ></textarea>
-                </div>
-                <button class="btn btn-primary" @click="registerMovie">
-                  Registrar
-                </button>
-              </div>
-            </b-sidebar>
-          </div>
+      <!-- Filtrado y búsqueda -->
+      <div class="row mb-3">
+        <div class="col-md-4">
+          <b-form-input v-model="searchQuery" placeholder="Buscar por nombre de película"></b-form-input>
         </div>
-
-        <div class="row">
-          <div
-            class="col-md-4"
-            v-for="(movie, index) in paginatedMovies"
-            :key="index"
-          >
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">{{ movie.title }}</h5>
-                <p class="card-text">
-                  <strong>Género:</strong> {{ movie.genre }}
-                </p>
-                <p class="card-text">
-                  <strong>Descripción:</strong> {{ movie.description }}
-                </p>
-              </div>
-            </div>
-          </div>
+        <div class="col-md-4">
+          <b-form-select v-model="selectedGenre" :options="genresOptions" :value="selectedGenre" placeholder="Todos los géneros"></b-form-select>
         </div>
+        <div class="col-md-4 text-right">
+          <b-button variant="primary" @click="showSidebar = true">
+            <b-icon icon="camera-reels-fill" class="mr-1"></b-icon>
+            Agregar
+          </b-button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4" v-for="(movie, index) in paginatedMovies" :key="index">
+          <b-card>
+            <b-card-body>
+              <h5 class="card-title">{{ movie.title }}</h5>
+              <p class="card-text"><strong>Género:</strong> {{ movie.genre }}</p>
+              <p class="card-text"><strong>Descripción:</strong> {{ movie.description }}</p>
+            </b-card-body>
+          </b-card>
+        </div>
+      </div>
 
-        <div class="row mt-4">
-          <div class="col-md-12">
-            <nav aria-label="Page navigation example">
-              <ul class="pagination justify-content-center">
-                <li
-                  class="page-item"
-                  v-for="page in totalPages"
-                  :key="page"
-                  :class="{ active: page === currentPage }"
-                >
-                  <a class="page-link" @click="changePage(page)">{{ page }}</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
+      <!-- Paginación -->
+      <div class="row mt-4">
+        <div class="col-md-12">
+          <b-pagination v-model="currentPage" :total-rows="filteredMovies.length" :per-page="moviesPerPage"></b-pagination>
         </div>
       </div>
     </section>
+    <b-alert v-model="showErrorAlert" variant="danger" dismissible fade @dismissed="showErrorAlert = false">
+      Ocurrió un error al obtener las películas. Por favor, inténtalo de nuevo más tarde.
+    </b-alert>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      movies: [
-        { title: 'Pelicula 1', genre: 'Aventura', description: 'Descripción de la Película 1' },
-        { title: 'Pelicula 2', genre: 'Comedia', description: 'Descripción de la Película 2' },
-        { title: 'Pelicula 3', genre: 'Drama', description: 'Descripción de la Película 3' },
-        { title: 'Pelicula 4', genre: 'Acción', description: 'Descripción de la Película 4' },
-        { title: 'Pelicula 5', genre: 'Terror', description: 'Descripción de la Película 5' },
-      ],
+      movies: [],
       currentPage: 1,
       moviesPerPage: 3,
       selectedGenre: null,
@@ -163,7 +87,8 @@ export default {
         title: '',
         genre: '',
         description: ''
-      }
+      },
+      showErrorAlert: false
     };
   },
   computed: {
@@ -181,56 +106,42 @@ export default {
       const startIndex = (this.currentPage - 1) * this.moviesPerPage;
       return this.filteredMovies.slice(startIndex, startIndex + this.moviesPerPage);
     },
-    totalPages() {
-      return Math.ceil(this.filteredMovies.length / this.moviesPerPage);
-    },
-    genres() {
-      return this.movies.reduce((acc, movie) => {
-        if (!acc.includes(movie.genre)) {
-          acc.push(movie.genre);
-        }
-        return acc;
-      }, []);
+    genresOptions() {
+      return [{ text: 'Todos los géneros', value: null }].concat(
+        [...new Set(this.movies.map(movie => movie.genre))]
+          .map(genre => ({ text: genre, value: genre }))
+      );
     }
   },
   methods: {
-    changePage(page) {
-      this.currentPage = page;
-    },
-    openSidebar() {
-      this.showSidebar = true;
-    },
-    registerMovie() {
-      // Validar que los campos no estén vacíos
-      if (this.newMovie.title && this.newMovie.genre && this.newMovie.description) {
-        // Agregar la nueva película al arreglo de películas
-        this.movies.push({
-          title: this.newMovie.title,
-          genre: this.newMovie.genre,
-          description: this.newMovie.description
-        });
-        // Limpiar los campos del formulario
-        this.newMovie.title = '';
-        this.newMovie.genre = '';
-        this.newMovie.description = '';
-        // Cerrar el sidebar después de agregar la película
+    async registerMovie() {
+      try {
+        const response = await axios.post('/api/movies/saveMovie', this.newMovie);
+        this.movies.push(response.data.payload);
+        this.resetForm();
         this.showSidebar = false;
-      } else {
-        alert('Por favor, complete todos los campos antes de registrar la película.');
+      } catch (error) {
+        console.error('Error al registrar película:', error);
+        this.showErrorAlert = true; 
       }
+    },
+    resetForm() {
+      this.newMovie.title = '';
+      this.newMovie.genre = '';
+      this.newMovie.description = '';
+    }
+  },
+  async created() {
+    try {
+      const response = await axios.get('/api/movies/');
+      this.movies = response.data.payload;
+    } catch (error) {
+      console.error('Error al obtener películas:', error);
+      this.showErrorAlert = true; 
     }
   }
 };
 </script>
 
-<style scoped>
-.btn-custom {
-  background-color: #ed6335;
-  border-color: #ed6335;
-}
-
-.btn-custom:hover {
-  background-color: #d65d31;
-  border-color: #d65d31;
-}
+<style>
 </style>
